c85b6af41b475461d9fed1da66b1d16371532eb3
Gabriel Felipe <tracerofphotons@gmail.com>
2021-06-08 06:04:42 -0300

    Pinneda Raster - Day 10: Rasterization SIMD Loop Otimization

--- src/raster_stage.c
+++ src/raster_stage.c
@@ -82,32 +82,38 @@ void RasterTriangles(SurfaceBuffer* sb, Triangle* tb, uint32_t tb_size)
 			b[3] = 0.0f;
 			c[3] = 0.0f;
 
+			__m128 const_a = _mm_load_ps(a);
+			__m128 const_b = _mm_load_ps(b);
+
 			//
-			// Edge Functions
+			// Edge Functions (Initial Value)
 			//
 			float e[4], curr_edge[4];
 
 			// (P2 - P1)
 			e[0] = a[0]*(x_min + 0.5f) + b[0]*(y_max - 0.5f) + c[0];
+			curr_edge[0] = e[0];
 
 			// (P0 - P2)
 			e[1] = a[1]*(x_min + 0.5f) + b[1]*(y_max - 0.5f) + c[1];
+			curr_edge[1] = e[1];
 
 			// (P1 - P0)
 			e[2] = a[2]*(x_min + 0.5f) + b[2]*(y_max - 0.5f) + c[2];
+			curr_edge[2] = e[2];
 
 			e[3] = 0.0f;
+			curr_edge[3] = e[3];
 
-			// loop (4 each time) SSE SIMD lanes
-			for (uint32_t i = y_max; i > y_min; --i) {
-				
-				// start at the left of the bounding-box
-				__m128 curr_e = _mm_load_ps(e);
-				__m128 curr_a = _mm_load_ps(a);
-				__m128 curr_b = _mm_load_ps(b);
-				_mm_store_ps((__m128*)curr_edge, curr_e);
+			__m128 curr_e = _mm_load_ps(e);
+			
+			//
+			// current performance without SIMD on DEBUG: 5ms (UPPER BOUND)
+			//
 
-				for (uint32_t j = x_min; j < x_max; j += 1u) {
+			// rasterizer inside the bounding-box
+			for (uint32_t i = y_max; i > y_min; i--) {
+				for (uint32_t j = x_min; j < x_max; j++) {
 					
 					//
 					// check if the pixels are inside/edge the triangle
@@ -116,11 +122,11 @@ void RasterTriangles(SurfaceBuffer* sb, Triangle* tb, uint32_t tb_size)
 					// curr_e > float4(0.0f)
 					__m128 mask_inside = _mm_cmpgt_ps(curr_e, _mm_setzero_ps());
 
-					// curr_a > float4(0.0f)
-					__m128 mask_edge_a = _mm_cmpgt_ps(curr_a, _mm_setzero_ps());
+					// const_a > float4(0.0f)
+					__m128 mask_edge_a = _mm_cmpgt_ps(const_a, _mm_setzero_ps());
 
-					// curr_b < float4(0.0f)
-					__m128 mask_edge_b = _mm_cmplt_ps(curr_b, _mm_setzero_ps());
+					// const_b < float4(0.0f)
+					__m128 mask_edge_b = _mm_cmplt_ps(const_b, _mm_setzero_ps());
 
 					// curr_e == float4(0.0f)
 					__m128 mask_edge_e = _mm_cmpeq_ps(curr_e, _mm_setzero_ps());
@@ -129,38 +135,50 @@ void RasterTriangles(SurfaceBuffer* sb, Triangle* tb, uint32_t tb_size)
 					__m128 mask_edge = _mm_and_ps(mask_edge_e, _mm_or_ps(mask_edge_a, mask_edge_b));
 
 					// final decision value
-					__m128 rasterize = _mm_and_ps(mask_inside, mask_edge);
-
+					mask_inside = _mm_and_ps(mask_inside, mask_inside);
+					__m128 rasterize = _mm_or_ps(mask_inside, mask_edge);
+					
 					//
 					// rasterize pixel in case IF and ONLY IF it passed in the test
 					//
 
 					// GREATE DOC TO KEEP STUDYING (PAGE 14)
-					http://www.cs.uu.nl/docs/vakken/magr/2017-2018/files/SIMD%20Tutorial.pdf
+					//http://www.cs.uu.nl/docs/vakken/magr/2017-2018/files/SIMD%20Tutorial.pdf
 
 
 					// barycentric coordinates
-					float l0 = curr_e0 / (tri_area2);
-					float l1 = curr_e1 / (tri_area2);
+					float lt[4] = { curr_edge[0] / (tri_area2), curr_edge[1] / (tri_area2), 1.0f, 0.0f };
+					__m128 l = _mm_load_ps(lt);
+				
+					float l0 = curr_edge[0] / (tri_area2);
+					float l1 = curr_edge[1] / (tri_area2);
 
 					// interpolate the color
+				
+					
 					float c_r = l0 * (tb[t].c0.x - tb[t].c2.x) + l1 * (tb[t].c1.x - tb[t].c2.x) + tb[t].c2.x;
 					float c_g = l0 * (tb[t].c0.y - tb[t].c2.y) + l1 * (tb[t].c1.y - tb[t].c2.y) + tb[t].c2.y;
 					float c_b = l0 * (tb[t].c0.z - tb[t].c2.z) + l1 * (tb[t].c1.z - tb[t].c2.z) + tb[t].c2.z;
 					Vec3 c = { c_r, c_g, c_b };
 
-					sb->surface_buffer[(sb->height - i) * sb->width + j] = rgb_float_to_uint32(c);
+					uint32_t address_mask = (rasterize.m128_u32[0] || rasterize.m128_u32[1] || rasterize.m128_u32[2]);
+					sb->surface_buffer[address_mask*(sb->height - i) * sb->width + j] = rgb_float_to_uint32(c);
 
 					// step edge functions in +x
 					curr_edge[0] += a[0];
 					curr_edge[1] += a[1];
 					curr_edge[2] += a[2];
+
+					curr_e = _mm_load_ps(curr_edge);
 				}
 
 				// step edge functions in -y
 				e[0] -= b[0];
 				e[1] -= b[1];
 				e[2] -= b[2];
+
+				curr_e = _mm_load_ps(e);
+				_mm_store_ps((__m128*)curr_edge, curr_e);
 			}
 
 
