cbf187cea14b83f1d43ac698a011cebf9f9a0e05
Gabriel Felipe <tracerofphotons@gmail.com>
2022-09-28 08:02:41 -0300

    Pinneda Raster - Day 23: SIMD Optimization
    Optimizing the m128i to uint32 ABGR packing.
    
    Gabriel

--- include/utils.h
+++ include/utils.h
@@ -63,23 +63,26 @@ static inline uint32_t rgb_float_to_uint32(Vec3 color_rgb)
 static inline uint32_t rgba_SIMD_float_to_uint32(__m128 color_rgba)
 {
     // Convert from float [0,1] to uint8 [0, 255] (Compression)
-    __m128 rgba_white = {0xFF, 0xFF, 0xFF, 0xFF};
+    __m128 rgba_white = _mm_set_ps(0xFF, 0xFF, 0xFF, 0xFF);
     __m128i int_rgba = _mm_cvtps_epi32(_mm_mul_ps(color_rgba, rgba_white));
 
-    // Pack each color to a byte in the first SIMD register (most signficative)
+    // Pack each color to a byte in the first SIMD register (lest signficative)
     __m128i R = _mm_slli_si128(int_rgba, 12 + 2);
-    __m128i G = _mm_slli_si128(int_rgba, 12 + 1);
+    __m128i G = _mm_slli_si128(int_rgba, 12 + 0);
     __m128i B = _mm_slli_si128(int_rgba, 12);
 
-    // Pack ARGB together in the first SIMD register (most signficative)
-    __m128i final_color = { 0x0, 0x0, 0x0, 0xFF000000 };
+    // Pack ARGB together in the first SIMD register (lest signficative)
+    __m128i final_color = _mm_setr_epi32(0x0, 0x0, 0x0, 0xFF000000);
     final_color = _mm_or_si128(final_color, R);
     final_color = _mm_or_si128(final_color, G);
     final_color = _mm_or_si128(final_color, B);
 
     // Return first SIMD register (most signficative)
-    uint32_t final_color_scalar = (uint32_t) _mm_cvtsi128_si32(final_color);
-    return final_color_scalar;
+    uint32_t final_color_scalar = (uint32_t) _mm_cvtsi128_si32(_mm_srli_si128(final_color, 12));
+    //return final_color_scalar;
+    uint32_t final_color_scalar2 = (uint32_t)(0xFF << 24 | int_rgba.m128i_u32[0] << 16 | int_rgba.m128i_u32[1] << 8 | int_rgba.m128i_u32[2]);
+    return final_color_scalar2;
+
     //return (uint32_t)(0xFF << 24 | int_rgba.m128i_u32[0] << 16 | int_rgba.m128i_u32[1] << 8 | int_rgba.m128i_u32[2]);
 }
 
